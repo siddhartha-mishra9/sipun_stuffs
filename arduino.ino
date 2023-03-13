6265868e36d4685845b24307b52956bf983ead82
siddhartha-mishra9 <68212137+siddhartha-mishra9@users.noreply.github.com>
2023-03-13 13:40:01 +0530

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -4,11 +4,14 @@ int microphone3Pin = A2;
 int microphone4Pin = A3;
 
 const int numReadings = 10;
+int idx = 0;
 
 int micreadings1[numReadings];
 int micreadings2[numReadings];
 int micreadings3[numReadings];
 int micreadings4[numReadings];
+float pastValuesAverage[1000]; 
+
 
 int targetValue = 0;
 int validResponse = 0;
@@ -18,6 +21,7 @@ long total1 = 0;
 long total2 = 0;
 long total3 = 0;
 long total4 = 0;
+long totalsmooth=0;
 
 int interval = 2000;
 long processedSensorValue;
@@ -39,13 +43,13 @@ void setup() {
   motorVibrate = 0;
 }
 
-
-
 void loop()
 {
   // Get clean sensor value
   processedSensorValue = smooth();
   
+  float newThreshold;
+
   // Set threshold dynamically
   // we constantly update the dynamic threshold. If we see that the baseline
   // average increases over a period of time, we update our current threshold
@@ -58,11 +62,9 @@ void loop()
         currentThreshold = sensorPastAverage;
     }
   }
- 
-  
-
   // what is this plotting. What is the response of the change.
-  Serial.println(processedSensorValue); 
+  Serial.println(processedSensorValue);
+  Serial.println(sensorPastAverage); 
 
   // Check threshold and vibrate motor
   if (processedSensorValue > currentThreshold)
@@ -82,13 +84,27 @@ void loop()
 
 bool hasCurrentThresholdDeviated()
 {
-
+  if(sensorPastAverage != currentThreshold)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 void updateSensorPastAverage(unsigned long sensorValue)
 {
-    // Keep taking average
-    
+  pastValuesAverage[idx]=sensorValue;
+  totalsmooth += totalsmoothpastValuesAverage[idx];
+  idx++;
+  if(idx == 1000)
+  {
+    idx=0;
+  }
+  sensorPastAverage=(totalsmooth / 1000);
+  
 }
 
 long smooth()
@@ -99,22 +115,17 @@ long smooth()
 	long average3;
 	long average4;
 
-    total1 = total1 - micreadings1[readIndex];
-	total2 = total2 - micreadings2[readIndex];
-	total3 = total3 - micreadings3[readIndex];
-	total4 = total4 - micreadings4[readIndex];
-
-    micreadings1[readIndex] = analogRead(microphone1Pin);
+  micreadings1[readIndex] = analogRead(microphone1Pin);
 	micreadings2[readIndex] = analogRead(microphone2Pin);
 	micreadings3[readIndex] = analogRead(microphone3Pin);
 	micreadings4[readIndex] = analogRead(microphone4Pin);
 
-    total1 = total1 + micreadings1[readIndex];
+  total1 = total1 + micreadings1[readIndex];
 	total2 = total2 + micreadings2[readIndex];
 	total3 = total3 + micreadings3[readIndex];
 	total4 = total4 + micreadings4[readIndex];
 
-    readIndex = readIndex + 1;
+  readIndex = readIndex + 1;
 
  	if (readIndex >= numReadings) {
  			readIndex = 0;
@@ -127,5 +138,6 @@ long smooth()
 
 	totaverage = (average1 + average2 + average3 + average4) / 4;
 
-    return totaverage;
+  return totaverage;
 }
+
